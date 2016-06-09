85c36b704243436eb4979b97f40c7fea7200f8ae
Carson Seese <cds17257@gmail.com>
2016-06-08 21:47:59 -0400

    Made mobile better :D

--- js/post-setup.js
+++ js/post-setup.js
@@ -1,6 +1,9 @@
 
-$(document).ready(function() {
-    $(".button-collapse").sideNav();
+$(document).ready(function () {
+    $('.button-collapse').sideNav({
+        closeOnClick: true 
+    });
+
     $(".parallax").parallax();
     window.onbeforeunload = function(){
 	    window.scrollTo(0,0);
