7a44742ac89d6e05fc51630b8873da3bec203e62
Josiah Nunemaker <yoshiya.d.n@gmail.com>
2016-09-04 12:07:53 -0500

    Finish maze

--- js/maze.js
+++ js/maze.js
@@ -12,7 +12,9 @@ var player = {
 };
 
 var boxes = [];
-
+var winPosX = 0;
+var winPosY = 4;
+var hasWon = false;
 var solids = [
   0, 0, 0, 1, 0,
   0, 1, 0, 0, 0,
@@ -38,11 +40,16 @@ function box(posX, posY, isPlayer, boxNumber) {
     ctx.fillStyle = "#000";
     ctx.fillRect(posX * 100, posY * 100, 100, 100);
     this.isSolid = true;
-  } else if (posX == player.posX && posY == player.posY) {
+  }
+  else if (posX == player.posX && posY == player.posY) {
     ctx.fillStyle = "#00c853";
     ctx.fillRect(posX * 100, posY * 100, 100, 100);
-  } else {
-    // ctx.strokeRect(posX * 100, posY * 100, 100, 100);
+  }
+  else if (posX == winPosX && posY == winPosY) {
+    ctx.fillStyle = "#00c853";
+    ctx.font = "24px 'Comic Sans MS', cursive, sans-serif";
+    ctx.textBaseline = "middle";
+    ctx.fillText("Home", (posX * 100) - (ctx.measureText("Home").width / 2) + 50, (posY * 100) + 50);
   }
 }
 
@@ -53,6 +60,11 @@ function coordsToBox(posX, posY) {
 function update() {
   ctx.clearRect(0, 0, 500, 500);
   ctx.strokeRect(0, 0, 500, 500);
+
+  if (player.posX == winPosX && player.posY == winPosY) {
+    winMessage();
+    return;
+  }
   for (i = 0; i < 25; i++) {
     // ctx.strokeRect((i % 5)*100, Math.floor((i / 5)) * 100, 100, 100);
     new box((i % 5), Math.floor((i / 5)), false, i);
@@ -63,35 +75,55 @@ window.addEventListener("keydown", function (event) {
   if (event.defaultPrevented) {
     return;
   }
-  // && boxes[coordsToBox(player.posX, player.posY - 1)].isSolid
-  switch (event.key) {
-  case "ArrowUp":
-    if (player.posY > 0 && !boxes[coordsToBox(player.posX, player.posY - 1)].isSolid) {
 
-      player.posY--;
+  if (!hasWon) {
+    switch (event.key) {
+    case "ArrowUp":
+      if (player.posY > 0 && !boxes[coordsToBox(player.posX, player.posY - 1)].isSolid) {
+
+        player.posY--;
+      }
+      break;
+    case "ArrowDown":
+      if (player.posY < 4 && !boxes[coordsToBox(player.posX, player.posY + 1)].isSolid) {
+        player.posY++;
+      }
+      break;
+    case "ArrowLeft":
+      if (player.posX > 0 && !boxes[coordsToBox(player.posX - 1, player.posY)].isSolid) {
+        player.posX--;
+      }
+      break;
+    case "ArrowRight":
+      if (player.posX < 4 && !boxes[coordsToBox(player.posX + 1, player.posY)].isSolid) {
+        player.posX++;
+      }
+      break;
+    default:
+      console.log("Unknown key: " + event.key);
+      return;
     }
-    break;
-  case "ArrowDown":
-    if (player.posY < 4 && !boxes[coordsToBox(player.posX, player.posY + 1)].isSolid) {
-      player.posY++;
-    }
-    break;
-  case "ArrowLeft":
-    if (player.posX > 0 && !boxes[coordsToBox(player.posX - 1, player.posY)].isSolid) {
-      player.posX--;
-    }
-    break;
-  case "ArrowRight":
-    if (player.posX < 4 && !boxes[coordsToBox(player.posX + 1, player.posY)].isSolid) {
-      player.posX++;
-    }
-    break;
-  default:
-    console.log("Unknown key: " + event.key);
-    return;
+
+    update();
+
+    event.preventDefault();
   }
-
-  update();
-
-  event.preventDefault();
 }, true);
+
+function winMessage() {
+  ctx.clearRect(0, 0, 500, 500);
+
+  console.log("You win!");
+  ctx.font = "48px impact";
+  ctx.textBaseline = "middle";
+  ctx.fillText("Congrats, you won!", 250 - (ctx.measureText("Congrats, you won!").width / 2), 250);
+
+  ctx.font = "24px monospace";
+  ctx.fillText("Going home...", 250 - (ctx.measureText("Going home...").width / 2), 400);
+
+  hasWon = true;
+  setTimeout(function () {
+    console.log("redirecting...");
+    window.location = "http://www.biohazard4050.org";
+  }, 3000);
+}
